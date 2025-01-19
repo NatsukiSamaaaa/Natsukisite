13abebf72b2bc7b922f856f248a39fc39690431b
NatsukiSamaaaa <kiyotakanatsuki@gmail.com>
2025-01-19 07:48:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Ambil elemen button
+const button1 = document.getElementById("clickMeButton1");
+const button2 = document.getElementById("clickMeButton2");
+const button3 = document.getElementById("clickMeButton3");
+const button4 = document.getElementById("clickMeButton4");
+
+// fungsi untuk tombol pertama
+button1.addEventListener("click", () => {
+    // arahkan ke situs kedua
+    window.location.href = "https://myanimelist.net/profile/N_Natsuki";
+});
+
+// fungsi untuk tombol kedua
+button2.addEventListener("click", () => {
+    // arahkan ke situs kedua
+    window.location.href = "https://myanimelist.net/animelist/N_Natsuki?view=tile&status=7"
+});
+
+// fungsi untuk tombol ketiga
+button3.addEventListener("click", () => {
+    // arahkan ke situs kedua
+    window.location.href = "https://www.tiktok.com/@natsukisamaaa"
+});
+
+// fungsi untuk tombol keempat
+button4.addEventListener("click", () => {
+    // arahkan ke situs kedua
+    window.location.href = "https://www.instagram.com/kiyotakanatsuki/"
+});
